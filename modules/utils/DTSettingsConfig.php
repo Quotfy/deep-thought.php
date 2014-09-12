a6cb9abb5ddddbd31871e78e0917e3bcad88060f
Blake Anderson <blake@expressiveanalytics.com>
2014-09-12 11:34:41 -0500

    bug fix

--- modules/utils/DTSettingsConfig.php
+++ modules/utils/DTSettingsConfig.php
@@ -35,7 +35,7 @@ class DTSettingsConfig extends DTSettings{
 	public static $shared_config = array();
 	
 	public static function initShared($path){
-		return static::$shared_config = json_decode(file_get_contents($path));
+		return static::$shared_config = json_decode(file_get_contents($path),true);
 	}
 	
 	public static function &sharedSettings(array $settings=null){
