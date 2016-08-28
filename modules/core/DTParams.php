4d9e654946099c1977bf01b7639dd9f02352ee64
Blake Anderson <blake@expressiveanalytics.com>
2016-08-28 18:17:47 -0500

    bug fix where null equals empty string

--- modules/core/DTParams.php
+++ modules/core/DTParams.php
@@ -11,10 +11,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -39,10 +39,10 @@ class DTParams implements arrayaccess{
 		$this->params = isset($params)?$params:$_REQUEST;
 		$this->db = isset($db)?$db:DTSettingsStorage::defaultStore();
 	}
-	
+
 	public function offsetSet($offset, $value) {
     	if (is_null($offset))
-            $this->params[] = $value; 
+            $this->params[] = $value;
         else
 			$this->params[$offset] = $value;
         return $value;
@@ -56,7 +56,7 @@ class DTParams implements arrayaccess{
     public function offsetGet($offset) {
     	return isset($this->params[$offset])?$this->params[$offset]:null;
     }
-	
+
 //======================
 //! Parameter Handling
 //======================
@@ -71,39 +71,39 @@ class DTParams implements arrayaccess{
 	public function jsonParam($name,$default=null){
 		return json_decode($this->param($name,$default),true);
 	}
-	
+
 	public function intParam($name,$default=null){
 		return intval($this->param($name,$default));
 	}
-	
+
 	public function doubleParam($name,$default=null){
 		return doubleval($this->param($name,$default));
 	}
-	
+
 	public function boolParam($name,$default=null){
 		return static::parseBool($this->param($name,$default));
 	}
-	
+
 	public function dateParam($name,$default=null){
 		$val = $this->param($name,$default);
 		return ($val=="")?null:$this->db->date(strtotime($val));
 	}
-	
+
 	public function timeParam($name,$default=null){
 		return $this->db->time(strtotime($this->param($name,$default)));
 	}
-	
+
 	public function dateTimeParams($dateName,$timeName,$dateDefault=null,$timeDefault=null){
 		$date = $this->param($dateName,$dateDefault);
 		if($date!=null)
 			return $this->db->date(strtotime($date." ".$this->param($timeName,$timeDefault)));
 		return null;
 	}
-	
+
 	public function checkboxParam($name){
 		return isset($this->params[$name])?1:0;
 	}
-	
+
 	public function phoneParam($name,$default=null){
 		if(preg_match("/(\d{3})?[^\d]*(\d{3})[^\d]*(\d{4})/",$this->param($name,$default),$matches)){
 			$area_code = ($matches[1]=="")?"":"({$matches[1]}) ";
@@ -111,7 +111,7 @@ class DTParams implements arrayaccess{
 		}
 		return "";
 	}
-	
+
 	/**
 		parse a valid email address
 		@return returns the email address or null if not a valid address
@@ -122,16 +122,16 @@ class DTParams implements arrayaccess{
 			return $str;
 		return null;
 	}
-	
+
 	public function arrayParam($name,$default=null){
 		return static::parseArray($this->param($name,$default),$this->db);
 	}
-	
+
 	/** @return returns a string param, cleaning it if +db+ is valid */
 	public function stringParam($name,$default=null){
 		return static::parseString($this->param($name,$default),$this->db);
 	}
-	
+
 	/** @return returns all parameters, using db cleaning */
 	public function allParams(array $defaults=array()){
 		$params = $defaults;
@@ -143,11 +143,11 @@ class DTParams implements arrayaccess{
 			else if($v instanceof SimpleXMLElement)
 				$params[$k] = $v; //pass XML children straight through
 			else
-				$params[$k] = static::parseString($v,$this->db);				
+				$params[$k] = static::parseString($v,$this->db);
 		}
 		return $params;
 	}
-	
+
 //====================
 //! Parse Methods
 //====================
@@ -187,7 +187,7 @@ class DTParams implements arrayaccess{
 		}
 		$out = array();
 		foreach($arr as $k=>$v) //clean all the array params
-			if($v==null)
+			if($v===null)
 				$out[$k] = "NULL";
 			else if(is_array($v))
 				$out[$k] = static::parseArray($v,$db); //recursively parse inner arrays
@@ -197,7 +197,7 @@ class DTParams implements arrayaccess{
 				$out[$k] = static::parseString($v,$db);
 		return $out;
 	}
-	
+
 	public static function parseString($val,$db){
 		return isset($db)?$db->clean($val):$val;
 	}
